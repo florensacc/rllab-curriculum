33af1c490b6888e87d4b83b37952b512cc089a6e
Rocky Duan <dementrock@gmail.com>
2015-11-21 17:07:46 -0800

    update
    Former-commit-id: 23b17a0bd4ff0beb6573ce12234e0332929a28bc

--- rllab/algo/npg.py
+++ rllab/algo/npg.py
@@ -3,7 +3,8 @@ import tensorfuse.tensor as TT
 import lasagne.updates
 from rllab.sampler import parallel_sampler
 from rllab.misc import logger, autoargs
-from rllab.misc.ext import extract, compact
+from rllab.misc.ext import extract, compact, merge_dict
+from rllab.misc.console import mkdir_p
 from rllab.algo.base import RLAlgorithm
 from functools import partial
 import numpy as np
@@ -82,7 +83,8 @@ class NPG(RLAlgorithm):
     @autoargs.arg('gae_lambda', type=float, help='Lambda used for generalized advantage estimation.')
     @autoargs.arg('learning_rate', type=float, help='Learning rate.')
     @autoargs.arg('plot', type=bool, help='Plot a test rollout per iteration.')
-    @autoargs.arg('whiten_advantage', type=bool, help='Whether to rescale and center the advantage values')
+    @autoargs.arg('whiten_advantage', type=bool, help='Whether to rescale and center the advantage values.')
+    @autoargs.arg('save_snapshot', type=bool, help='Whether to save parameters for each iteration.')
     def __init__(
             self,
             max_path_length=np.inf,
@@ -94,7 +96,8 @@ class NPG(RLAlgorithm):
             discount=0.99,
             gae_lambda=1,
             plot=False,
-            whiten_advantage=True
+            whiten_advantage=True,
+            save_shapshot=True
             ):
         self.max_path_length = max_path_length
         self.batch_size = batch_size
@@ -108,6 +111,7 @@ class NPG(RLAlgorithm):
         self.gae_lambda = gae_lambda
         self.plot = plot
         self.whiten_advantage = whiten_advantage
+        self.save_snapshot = save_snapshot
 
     def start_worker(self, mdp, policy, vf):
         parallel_sampler.populate_task(mdp, policy)
@@ -123,6 +127,8 @@ class NPG(RLAlgorithm):
             logger.push_prefix('itr #%d | ' % itr)
             samples_data = self.obtain_samples(itr, mdp, policy, vf)
             opt_info = self.optimize_policy(itr, policy, samples_data, opt_info)
+            if self.save_snapshot:
+                self.perform_save_snapshot(itr, samples_data, opt_info)
             logger.dump_tabular(with_prefix=False)
             logger.pop_prefix()
 
@@ -147,15 +153,12 @@ class NPG(RLAlgorithm):
                 return (qval - np.mean(qval)) / (qval.std() + 1e-8)
 
             results["all_advantages"] = center_qval(results["all_advantages"])
+        results["vf_params"] = vf.get_param_values()
         return results
 
     def init_opt(self, mdp, policy, vf):
         train_vars = get_train_vars(policy)
         surr_obj, fisher_grads = new_opt_vals(policy, **train_vars)
-        #grads = theano.grad(surr_obj, policy.params)
-        #emp_fisher = policy.get_empirical_fisher(
-        #f_emp_fisher = grad
-        #grads
         updates = self.update_method(fisher_grads, policy.params)
         input_list = to_input_var_list(**train_vars)
         f_update = theano.function(
@@ -178,6 +181,7 @@ class NPG(RLAlgorithm):
         )
 
     def optimize_policy(self, itr, policy, samples_data, opt_info):
+        cur_params = policy.get_param_values()
         logger.log("optimizing policy")
         f_update = opt_info["f_update"]
         f_loss = opt_info["f_loss"]
@@ -185,9 +189,29 @@ class NPG(RLAlgorithm):
         logger.log("computing loss before")
         loss_before = f_loss(*all_inputs)
         # Need to ensure this
-        opt_info["ref_policy"].set_param_values(policy.get_param_values())
+        opt_info["ref_policy"].set_param_values(cur_params)
         f_update(*all_inputs)
         loss_after = f_loss(*all_inputs)
         logger.record_tabular("Loss Before", loss_before)
         logger.record_tabular("Loss After", loss_after)
-        return opt_info
+        opt_params = policy.get_param_values()
+        return merge_dict(opt_info, dict(
+            cur_params=cur_params,
+            opt_params=opt_params,
+        ))
+
+    def perform_save_snapshot(self, itr, samples_data, opt_info):
+        if self.save_snapshot:
+            logger.log("saving result...")
+            savedir = 'data/%s' % (self.exp_name)
+            mkdir_p(savedir)
+            to_save = {
+                'itr': itr,
+                'cur_policy_params': opt_info['cur_params'],
+                'opt_policy_params': opt_info['opt_params'],
+                'vf_params': samples_data['vf_params'],
+                'all_obs': samples_data['all_obs'],
+                'all_advantages': samples_data['all_advantages'],
+                'actions': samples_data['all_actions'],
+            }
+            np.savez_compressed('%s/itr_%03d.npz' % (savedir, itr), **to_save)
--- scripts/sim_policy.py
+++ scripts/sim_policy.py
@@ -1,19 +1,21 @@
 import os
-os.environ['TENSORFUSE_MODE'] = 'theano'
-os.environ['THEANO_FLAGS'] = 'device=cpu'
 import numpy as np
 from rllab.policy.mujoco_policy import MujocoPolicy
-from rllab.mdp.mujoco.ant_mdp import AntMDP
+from rllab.mdp.john_mjc2 import SwimmerMDP#.ant_mdp import AntMDP
 #from rllab.mdp.igor_mjc import AcrobotMDP
 from rllab.sampler.utils import rollout
 
+import sys
+import argparse
+
+
 print 'reading data'
-data = np.load('itr_033.npz')
+data = np.load('itr_014.npz')
 print 'read data'
 
 params = data['cur_policy_params']
 print params.shape
-mdp = AntMDP()
+mdp = SwimmerMDP()
 policy = MujocoPolicy(mdp, hidden_sizes=[32, 32])#30,30])
 print policy.get_param_values().shape
 policy.set_param_values(params)
@@ -21,6 +23,6 @@ policy.set_param_values(params)
 #policy.log_std_vars[0].set_value(np.ones_like(policy.log_std_vars[0].get_value()) * -100)
 #cur_params = policy.get_param_values()
 result = rollout(mdp, policy, max_length=1000, animated=True)#mdp.demo_policy(policy)
-import ipdb; ipdb.set_trace()
+#import ipdb; ipdb.set_trace()
 
 
