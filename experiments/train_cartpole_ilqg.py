62d130a01d9448e0df62c1acb9d8ea7e16148a94
Rocky Duan <dementrock@gmail.com>
2015-11-21 17:08:23 -0800

    cleanup
    Former-commit-id: 5cade7d58772d7aad72ee96d057c5fbcfc06b1a7

--- experiments/train_cartpole_ilqg.py
+++
@@ -1,6 +0,0 @@
-from rllab.algo.ilqg import ILQG
-from rllab.mdp.cartpole_mdp import CartpoleMDP
-
-mdp = CartpoleMDP()
-mdp.horizon = 30
-ILQG().train(mdp)
--- experiments/train_swimmer_sqp.py
+++
@@ -1,6 +0,0 @@
-from algo.sqp import SQP
-from mdp.swimmer_mdp import SwimmerMDP
-
-mdp = SwimmerMDP()
-mdp.horizon = 50
-SQP().train(mdp)
