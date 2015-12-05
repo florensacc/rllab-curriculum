6d17d93a27830de1df81dc7637bd5926928f106c
Rocky Duan <dementrock@gmail.com>
2015-12-05 12:22:02 -0800

    improve sim policy code
    Former-commit-id: cf9e40b5207acba0d72e2cf8afeb2231dba68994

--- scripts/sim_policy.py
+++ scripts/sim_policy.py
@@ -1,28 +1,16 @@
-import os
-import numpy as np
-from rllab.policy.mujoco_policy import MujocoPolicy
-from rllab.mdp.john_mjc2 import SwimmerMDP#.ant_mdp import AntMDP
-#from rllab.mdp.igor_mjc import AcrobotMDP
 from rllab.sampler.utils import rollout
-
-import sys
 import argparse
+import joblib
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str,
+                        help='path to the snapshot file')
+    parser.add_argument('--max_length', type=int, default=1000,
+                        help='Max length of rollout')
 
-print 'reading data'
-data = np.load('itr_014.npz')
-print 'read data'
-
-params = data['cur_policy_params']
-print params.shape
-mdp = SwimmerMDP()
-policy = MujocoPolicy(mdp, hidden_sizes=[32, 32])#30,30])
-print policy.get_param_values().shape
-policy.set_param_values(params)
-# zero out the variance
-#policy.log_std_vars[0].set_value(np.ones_like(policy.log_std_vars[0].get_value()) * -100)
-#cur_params = policy.get_param_values()
-result = rollout(mdp, policy, max_length=1000, animated=True)#mdp.demo_policy(policy)
-#import ipdb; ipdb.set_trace()
-
-
+    args = parser.parse_args()
+    data = joblib.load(args.file)
+    policy = data['policy']
+    mdp = data['mdp']
+    rollout(mdp, policy, max_length=args.max_length, animated=True)
