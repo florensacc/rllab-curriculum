a35f4b53ceb894f38443ec9f4f756e8917b31cca
Rocky Duan <dementrock@gmail.com>
2015-12-19 22:44:12 -0800

    get rid of dependency on rlglue
    Former-commit-id: e711b2f675416dd0d53f94abac2dc036e9993f66

--- rllab/mdp/mountain_car_mdp.py
+++ rllab/mdp/mountain_car_mdp.py
@@ -14,18 +14,13 @@
 # limitations under the License.
 #
 
-import numpy
+import numpy as np
 
-from rlglue.environment.Environment import Environment
-from rlglue.environment import EnvironmentLoader as EnvironmentLoader
-from rlglue.types import Observation
-from rlglue.types import Action
-from rlglue.types import Reward_observation_terminal
 from rllab.mdp.base import MDP
 from rllab.misc.overrides import overrides
 
 
-class MountainCarND(Environment,object):
+class MountainCarND(object):
     """A generalized Mountain Car domain, which allows N-dimensional
     movement. When dimension=2 this behaves exactly as the classical
     Mountain Car domain. For dimension=3 it behaves as given in the
@@ -37,13 +32,15 @@ class MountainCarND(Environment,object):
     However, this class also allows for even greater dimensions.
     """
     name = "3D Mountain Car"
-    def __init__(self, **kwargs):#noise=0.0, random_start=False, dim=2):
+
+    def __init__(self, **kwargs):
         dim = int(max(2, kwargs.setdefault('dimension', 3)))
         self.noise = kwargs.setdefault('noise', 0.0)
         self.reward_noise = kwargs.setdefault('reward_noise', 0.0)
         self.random_start = kwargs.setdefault('random_start', False)
-        self.state = numpy.zeros((dim-1,2))
-        self.state_range = numpy.array([[[-1.2, 0.6], [-0.07, 0.07]] for i in range(dim-1)])
+        self.state = np.zeros((dim-1, 2))
+        self.state_range = np.array(
+            [[[-1.2, 0.6], [-0.07, 0.07]] for i in range(dim-1)])
         self.goalPos = 0.5
         self.acc = 0.001
         self.gravity = -0.0025
@@ -52,65 +49,53 @@ class MountainCarND(Environment,object):
 
     def reset(self):
         if self.random_start:
-            self.state = numpy.random.random(self.state.shape)
-            self.state *= (self.state_range[:,:,1] - self.state_range[:,:,0])
-            self.state += self.state_range[:,:,0]
+            self.state = np.random.random(self.state.shape)
+            self.state *= (self.state_range[:, :, 1] -
+                           self.state_range[:, :, 0])
+            self.state += self.state_range[:, :, 0]
         else:
-            self.state = numpy.zeros(self.state.shape)
-            self.state[:,0] = -0.5
-
-    def env_start(self):
-        self.reset()
-        returnObs = Observation()
-        returnObs.doubleArray = self.state.flatten().tolist()
-        return returnObs
+            self.state = np.zeros(self.state.shape)
+            self.state[:, 0] = -0.5
 
     def isAtGoal(self):
-        return (self.state[:,0] >= self.goalPos).all()
+        return (self.state[:, 0] >= self.goalPos).all()
 
     def takeAction(self, intAction):
         # Translate action into a (possibly) multi-dimensional direction
-        intAction -= 1
-        direction = numpy.zeros((self.state.shape[0],)) # Zero is Neutral
+        direction = np.zeros((self.state.shape[0],))  # Zero is Neutral
         if intAction >= 0:
             direction[int(intAction)/2] = ((intAction % 2) - 0.5)*2.0
         if self.noise > 0:
-            direction += self.acc * numpy.random.normal(scale=self.noise, size=direction.shape)
+            direction += self.acc * np.random.normal(
+                scale=self.noise, size=direction.shape)
 
-        self.state[:,1] += self.acc*(direction) + self.gravity*numpy.cos(self.hillFreq*self.state[:,0])
-        self.state[:,1] = self.state[:,1].clip(min=self.state_range[:,1,0], max=self.state_range[:,1,1])
-        self.state[:,0] += self.delta_time * self.state[:,1]
-        self.state[:,0] = self.state[:,0].clip(min=self.state_range[:,0,0], max=self.state_range[:,0,1])
+        self.state[:, 1] += \
+            self.acc*(direction) + \
+            self.gravity*np.cos(self.hillFreq*self.state[:, 0])
+        self.state[:, 1] = self.state[:, 1].clip(
+            min=self.state_range[:, 1, 0], max=self.state_range[:, 1, 1])
+        self.state[:, 0] += self.delta_time * self.state[:, 1]
+        self.state[:, 0] = self.state[:, 0].clip(
+            min=self.state_range[:, 0, 0], max=self.state_range[:, 0, 1])
 
-    def env_step(self,thisAction):
-        episodeOver = 0
-        theReward = -1.0
-        intAction = thisAction.intArray[0]
+    def step(self, action):
+        done = False
+        reward = -1.0
+        # intAction = thisAction.intArray[0]
 
-        self.takeAction(intAction)
+        self.takeAction(action)
 
         if self.isAtGoal():
-            theReward = 0.0
-            episodeOver = 1
+            reward = 0.0
+            done = True
 
         if self.reward_noise > 0:
-            theReward += numpy.random.normal(scale=self.reward_noise)
+            reward += np.random.normal(scale=self.reward_noise)
 
-        theObs = Observation()
-        theObs.doubleArray = self.state.flatten().tolist()
+        next_state = np.copy(self.state).flatten(-1)
+        next_obs = np.copy(next_state)
+        return next_state, next_obs, reward, done
 
-        returnRO = Reward_observation_terminal()
-        returnRO.r = theReward
-        returnRO.o = theObs
-        returnRO.terminal = episodeOver
-
-        return returnRO
-
-    def env_cleanup(self):
-        pass
-
-    def env_message(self,inMessage):
-        return "I don't know how to respond to your message";
 
 class MountainCar(MountainCarND):
     name = "Mountain Car"
@@ -153,22 +138,4 @@ class MountainCarMDP(MDP):
 
     @overrides
     def step(self, state, action):
-        int_action = action + 1
-        act = Action()
-        act.intArray = [int_action]
-        ret = self._mc.env_step(act)
-        next_state = numpy.copy(self._mc.state)
-        next_obs = numpy.copy(self._mc.state)
-        reward = ret.r
-        done = ret.terminal
-        return next_state.reshape(-1), next_obs.reshape(-1), reward, done
-
-
-# if __name__=="__main__":
-#     import argparse
-#     parser = argparse.ArgumentParser(description='Run Noisy Mountain Car environment in network mode.')
-#     parser.add_argument("--noise", type=float, default=0, help="Standard deviation of additive noise to generate, affects the action effects.")
-#     parser.add_argument("--random_restarts", type=bool, default=False, help="Restart the cart with a random location and velocity.")
-# 
-#     args = parser.parse_args()
-#     EnvironmentLoader.loadEnvironment(MountainCar(noise=args.noise, random_start=args.random_restarts))
+        return self._mc.step(action)
