804853aeee92e17fd12ced91b6c11b1dc468f11e
Rocky Duan <dementrock@gmail.com>
2016-01-18 07:11:01 +0000

    update
    Former-commit-id: 5fc4758c83529c60ec5ba7e3de4f9dea172d51b0

--- snippets/rocky/run_dpg_mujoco_pre_2_cheetah.rb
+++ snippets/rocky/run_dpg_mujoco_pre_2_cheetah.rb
@@ -1,10 +1,14 @@
 require_relative './utils'
 
-qf_learning_rates = [1e-3, 1e-4, 1e-5, 1e-6]
-policy_learning_rates = [1e-3, 1e-4, 1e-5, 1e-6]
+qf_learning_rates = [1e-4, 1e-5, 5e-5, 1e-6]
+policy_learning_rates = [1e-4, 1e-5, 5e-5, 1e-6]
 seeds = [1, 2, 3]
 
-shuffle_params(qf_learning_rates, policy_learning_rates, seeds).each do |qf_learning_rate, policy_learning_rate, seed|
+hidden_sizess = [[32, 32], [400, 300]]
+qf_weight_decays = [0, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]
+soft_target_taus = [1e-4, 1e-3, 1e-2, 1e-1, 1]
+
+shuffle_params(qf_learning_rates, policy_learning_rates, seeds, hidden_sizess, qf_weight_decays, soft_target_taus).each do |qf_learning_rate, policy_learning_rate, seed, hidden_sizes, qf_weight_decay, soft_target_tau|
 
   params = {
     mdp: {
@@ -13,11 +17,11 @@ shuffle_params(qf_learning_rates, policy_learning_rates, seeds).each do |qf_lear
     normalize_mdp: true,
     qf: {
       _name: "continuous_nn_q_function",
-      hidden_sizes: [32, 32],
+      hidden_sizes: hidden_sizes, # [32, 32],
     },
     policy: {
       _name: "mean_nn_policy",
-      hidden_sizes: [32, 32],
+      hidden_sizes: hidden_sizes, # [32, 32],
       hidden_nl: 'lasagne.nonlinearities.rectify',
       output_nl: 'lasagne.nonlinearities.tanh',
       output_W_init: 'lasagne.init.Uniform(-3e-3, 3e-3)',
@@ -26,18 +30,18 @@ shuffle_params(qf_learning_rates, policy_learning_rates, seeds).each do |qf_lear
     },
     algo: {
       _name: "dpg",
-      batch_size: 32,
-      n_epochs: 25,
+      batch_size: 64,
+      n_epochs: 100,
       epoch_length: 10000,
       min_pool_size: 10000,
-      replay_pool_size: 250000,
+      replay_pool_size: 1000000,
       discount: 0.99,
-      qf_weight_decay: 0,
+      qf_weight_decay: qf_weight_decay,
       qf_learning_rate: qf_learning_rate,
       max_path_length: 100,
       eval_samples: 1,
       eval_whole_paths: true,
-      soft_target_tau: 0.001,
+      soft_target_tau: soft_target_tau,
       policy_learning_rate: policy_learning_rate,
     },
     es: {
