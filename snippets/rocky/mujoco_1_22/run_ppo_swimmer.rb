881dae7c4243d5abe2603f722f4d53f5a499bba8
Rocky Duan <dementrock@gmail.com>
2016-01-19 11:09:13 -0800

    update
    Former-commit-id: 50fe4d472c35814812247853f57698444a11697e

---
+++ snippets/rocky/mujoco_1_22/run_ppo_swimmer.rb
@@ -0,0 +1,33 @@
+require_relative '../utils'
+
+params = {
+  mdp: {
+    _name: "mujoco_1_22.swimmer_mdp",
+  },
+  normalize_mdp: true,
+  policy: {
+    _name: "mean_std_nn_policy",
+    hidden_sizes: [32, 32],
+  },
+  baseline: {
+    _name: "linear_feature_baseline",
+  },
+  algo: {
+    _name: "ppo",
+    whole_paths: true,
+    batch_size: 50000,
+    max_path_length: 500,
+    n_itr: 500,
+    binary_search_penalty: false,
+    # bs_kl_tolerance: 0.001,
+    step_size: 0.01,
+    plot: true,
+  },
+  n_parallel: 4,
+  snapshot_mode: "all",
+  seed: 1,
+  plot: true,
+}
+command = to_command(params)
+puts command
+system(command)
