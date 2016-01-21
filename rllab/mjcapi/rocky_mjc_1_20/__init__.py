766c10862bfbf8c38f05565a6c7b69ea249e4df8
Rocky Duan <dementrock@gmail.com>
2016-01-21 10:07:44 -0800

    clean up: mujoco 1.20 not needed
    Former-commit-id: 1e0648e5f8143fd109e10261d9ffec28188ddf65

--- rllab/mjcapi/rocky_mjc_1_20/__init__.py
+++
@@ -1,9 +0,0 @@
-from .mjviewer import MjViewer
-from .mjcore import MjModel
-from .mjcore import register_license
-import os
-from mjconstants import *
-
-register_license(os.path.join(os.path.dirname(__file__),
-                              '../../../vendor/mujoco/1_20/osx/LICENSE_DEEPRL.TXT'))
-
--- rllab/mjcapi/rocky_mjc_1_20/gen_binding.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
-gcc -E -CC $parent_path/../../../vendor/mujoco/1_20/osx/mujoco.h > /tmp/code_gen_mujoco.h
-ruby $parent_path/codegen.rb /tmp/code_gen_mujoco.h > $parent_path/mjtypes.py
