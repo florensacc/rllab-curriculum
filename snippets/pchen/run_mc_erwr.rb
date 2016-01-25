cf6ced960d2000920529d8cf791efdc9c9b00c96
peter chen <pchen@sellegit.com>
2016-01-25 08:49:01 -0800

    tweak
    Former-commit-id: 781504bf978f0e52dfaec5a0880c0000ead3dbf2

--- snippets/pchen/run_mc_erwr.rb
+++ snippets/pchen/run_mc_erwr.rb
@@ -8,6 +8,7 @@ params = {
     # _name: "box2d.mountain_car_mdp",
     _name: "box2d.cartpole_mdp",
   },
+  normalize_mdp: true,
   policy: {
     _name: "mean_std_nn_policy",
     std_trainable: true,
@@ -16,14 +17,14 @@ params = {
     std_sizes: []
   },
   baseline: {
-    # _name: "zero_baseline",
-    _name: "linear_feature_baseline",
+    _name: "zero_baseline",
+    # _name: "linear_feature_baseline",
   },
   exp_name: "erwr_mc_seed_#{seed}",
   algo: {
-    _name: "ppo",
-    step_size: 0.1,
-    binary_search_penalty: false,
+    # _name: "ppo",
+    # step_size: 0.1,
+    # binary_search_penalty: false,
 
     # _name: "trpo",
     # step_size: 0.2,
@@ -33,12 +34,12 @@ params = {
 
     # _name: "cem",
 
-    # _name: "erwr",
-    # max_opt_itr: 50,
-    # # best_quantile: 0.1,
+    _name: "erwr",
+    max_opt_itr: 50,
+    # best_quantile: 0.1,
 
-    center_adv: true,
-    batch_size: 10000,
+    center_adv: false,
+    batch_size: 5000,
     whole_paths: true,
     max_path_length: 100,
     n_itr: 500,
