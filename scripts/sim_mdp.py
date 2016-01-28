8ce3e27ef7788e89be7cc256932c8718de54375d
peter chen <pchen@sellegit.com>
2016-01-28 14:44:44 -0800

    staged failed attempt at inverted double pendulum with box2d
    Former-commit-id: c19ee1954b5a66ca736351e37b718443b3235af7

--- scripts/sim_mdp.py
+++ scripts/sim_mdp.py
@@ -69,6 +69,7 @@ def visualize_mdp(mdp, mode, max_steps=sys.maxint, fps=20):
             tr += r
             mdp.plot()
             time.sleep(delay)
+            print "reward:", r
             if done:
                 print "Episode done, reward: ", tr
                 tr = 0.
