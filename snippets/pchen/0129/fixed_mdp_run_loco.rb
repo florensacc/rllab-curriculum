5cd0610522128dff9a0a1bdd1035d166c39b0bde
peter chen <pchen@sellegit.com>
2016-01-29 14:55:37 -0800

    exp
    Former-commit-id: 0af8232a5e7d1f1dca8ad4b8d69bdc25f7e15611

--- snippets/pchen/0129/fixed_mdp_run_loco.rb
+++ snippets/pchen/0129/fixed_mdp_run_loco.rb
@@ -7,46 +7,57 @@ discount = 0.99
 seeds = (1..5).each do |i| i ** 2 * 5 + 23 end
 
 mdps = []
-mdps << "mujoco_1_22.half_cheetah_mdp"
+# mdps << "mujoco_1_22.half_cheetah_mdp"
+mdps << "mujoco_1_22.full_cheetah_mdp"
 mdps << "mujoco_1_22.hopper_mdp"
-mdps << "mujoco_1_22.swimmer_mdp"
+# mdps << "mujoco_1_22.swimmer_mdp"
 # mdps << "mujoco_1_22.humanoid_mdp"
 
 algos = []
 # # erwr
-# [0.2, 1].each do |best_quantile|
-#   algos << {
-#     _name: "erwr",
-#     max_opt_itr: 50,
-#     best_quantile: best_quantile,
-#     positive_adv: true,
-#   }
-# end
+[0.2, 1].each do |best_quantile|
+  algos << {
+    _name: "erwr",
+    max_opt_itr: 50,
+    best_quantile: best_quantile,
+    positive_adv: true,
+  }
+end
 # # # trpo & ppo
-# [0.01, 0.1].each do |ss|
-#   algos << {
-#     _name: "ppo",
-#     step_size: ss,
-#   }
-# end
-# [0.1, 1].each do |ss|
-#   algos << {
-#     _name: "trpo",
-#     step_size: ss,
-#     backtrack_ratio: 0.8,
-#   }
-# end
+[0.01, 0.1].each do |ss|
+  algos << {
+    _name: "ppo",
+    step_size: ss,
+  }
+end
+[0.1, 1].each do |ss|
+  algos << {
+    _name: "trpo",
+    step_size: ss,
+    backtrack_ratio: 0.8,
+  }
+end
 # # # npg
-# [0.1, 0.01].each do |ss|
-#   [1e-1, 1e0].each do |lr|
-#     algos << {
-#       _name: "npg",
-#       step_size: ss,
-#       update_method: "adam",
-#       learning_rate: lr,
-#     }
-#   end
-# end
+[0.1, 0.01].each do |ss|
+  [1e-1, 1e0, 1e-2].each do |lr|
+    algos << {
+      _name: "npg",
+      step_size: ss,
+      update_method: "adam",
+      learning_rate: lr,
+    }
+  end
+end
+[0.1, 0.01].each do |ss|
+  [1e-1, 1e0, 1e-2].each do |lr|
+    algos << {
+      _name: "npg",
+      step_size: ss,
+      update_method: "sgd",
+      learning_rate: lr,
+    }
+  end
+end
 # # vpg
 [1e-4, 1e-3, 1e-2, 1e-1].each do |lr|
   algos << {
@@ -82,7 +93,7 @@ inc = 0
 seeds.each do |seed|
   mdps.each do |mdp|
     algos.each do |algo|
-      exp_name = "vpg_fixed_0129_nn_pi_loco_#{inc = inc + 1}"
+      exp_name = "mdp_fixed_0129_nn_pi_loco_#{inc = inc + 1}"
       params = {
         mdp: {
           _name: mdp,
