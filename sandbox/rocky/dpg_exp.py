de844554595d458b73a912d6ac7e2ae4b0bc1143
Rocky Duan <dementrock@gmail.com>
2016-04-02 14:46:10 -0700

    launch new experiments
    Former-commit-id: e69c267bc198c50c180a3223029eefdf22ed0721

--- sandbox/rocky/dpg_exp.py
+++ sandbox/rocky/dpg_exp.py
@@ -1,5 +1,16 @@
-from rllab.algos.dpg import DPG
+from rllab.algos.ddpg import DDPG
+from rllab.envs.box2d.cartpole_env import CartpoleEnv
+from rllab.envs.box2d.cartpole_swingup_env import CartpoleSwingupEnv
+from rllab.envs.box2d.mountain_car_env import MountainCarEnv
+from rllab.envs.box2d.double_pendulum_env import DoublePendulumEnv
 from rllab.envs.mujoco.swimmer_env import SwimmerEnv
+from rllab.envs.mujoco.hopper_env import HopperEnv
+from rllab.envs.mujoco.walker2d_env import Walker2DEnv
+from rllab.envs.mujoco.ant_env import AntEnv
+from rllab.envs.mujoco.inverted_double_pendulum_env import InvertedDoublePendulumEnv
+from rllab.envs.mujoco.simple_humanoid_env import SimpleHumanoidEnv
+from rllab.envs.mujoco.humanoid_env import HumanoidEnv
+from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
 from rllab.envs.normalized_env import normalize
 from rllab.exploration_strategies.ou_strategy import OUStrategy
 from rllab.misc import instrument
@@ -11,18 +22,18 @@ instrument.stub(globals())
 vg = instrument.VariantGenerator()
 vg.add("seed", [1, 11, 21, 31, 41])
 vg.add("env", map(normalize, [
-    # CartpoleEnv(),
-    # CartpoleSwingupEnv(),
-    # MountainCarEnv(),
-    # DoublePendulumEnv(),
+    CartpoleEnv(),
+    CartpoleSwingupEnv(),
+    MountainCarEnv(),
+    DoublePendulumEnv(),
     SwimmerEnv(),
-    # HopperEnv(),
-    # Walker2DEnv(),
-    # HalfCheetahEnv(),
-    # AntEnv(),
-    # InvertedDoublePendulumEnv(),
-    # SimpleHumanoidEnv(),
-    # HumanoidEnv(),
+    HopperEnv(),
+    Walker2DEnv(),
+    HalfCheetahEnv(),
+    AntEnv(),
+    InvertedDoublePendulumEnv(),
+    SimpleHumanoidEnv(),
+    HumanoidEnv(),
 ]))
 vg.add("es", lambda env: [OUStrategy(env_spec=env.spec, theta=0.15, sigma=0.3)])
 vg.add("qf_weight_decay", [0.])  # , 0.01])
@@ -34,38 +45,42 @@ vg.add("bn", [False])
 
 print "#Experiments:", len(vg.variants())
 for variant in vg.variants():
-    algo = DPG(
-        qf_learning_rate=variant["qf_lr"],
-        qf_weight_decay=variant["qf_weight_decay"],
-        policy_learning_rate=variant["policy_lr"],
-        max_path_length=100,
-        min_pool_size=100,
-        epoch_length=100,
-        n_epochs=2500,
-        scale_reward=variant["scale_reward"],
-        soft_target_tau=variant["soft_target_tau"],
-        eval_samples=100,
-        # n_epochs=50,
-    )
     policy = DeterministicMLPPolicy(
         env_spec=variant["env"].spec,
-        hidden_sizes=(8,),
+        hidden_sizes=(400, 300),
         bn=variant["bn"],
     )
     qf = ContinuousMLPQFunction(
         env_spec=variant["env"].spec,
-        hidden_sizes=(8,),
+        hidden_sizes=(400, 300),
         bn=variant["bn"],
     )
+    algo = DDPG(
+        env=variant["env"],
+        policy=policy,
+        qf=qf,
+        es=variant["es"],
+        qf_learning_rate=variant["qf_lr"],
+        qf_weight_decay=variant["qf_weight_decay"],
+        policy_learning_rate=variant["policy_lr"],
+        max_path_length=500,
+        min_pool_size=10000,
+        epoch_length=10000,
+        n_epochs=2500,
+        scale_reward=variant["scale_reward"],
+        soft_target_tau=variant["soft_target_tau"],
+        eval_samples=10000,
+        # n_epochs=50,
+    )
     instrument.run_experiment_lite(
-        algo.train(env=variant["env"], policy=policy, qf=qf, es=variant["es"]),
-        exp_prefix="dpg_all_search_fixed_single_test",
+        algo.train(),
+        exp_prefix="dpg_new_search",
         n_parallel=4,
         snapshot_mode="last",
         seed=variant["seed"],
-        # mode="ec2",
+        mode="ec2",
         # terminate_machine=False,
         # dry=True,
     )
-    break
+    # break
     # break
