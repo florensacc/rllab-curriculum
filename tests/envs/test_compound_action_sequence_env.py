533d0a500c1ea6344fba8e00dd47d5114162a523
Rocky Duan <dementrock@gmail.com>
2016-04-09 18:44:41 -0700

    fix test
    Former-commit-id: ab9685788edfb760d21430c6443b7c0c28e5fe41

--- tests/envs/test_compound_action_sequence_env.py
+++ tests/envs/test_compound_action_sequence_env.py
@@ -6,7 +6,7 @@ from nose2 import tools
 def test_compound_action_sequence_env():
     from rllab.envs.grid_world_env import GridWorldEnv
     from rllab.envs.compound_action_sequence_env import CompoundActionSequenceEnv
-    mdp = GridWorldEnv(desc=[
+    env = GridWorldEnv(desc=[
         "SFFF",
         "FFFF",
         "FFFF",
@@ -14,23 +14,24 @@ def test_compound_action_sequence_env():
     ])
     # this should succeed
     CompoundActionSequenceEnv(
-        wrapped_env=mdp,
-        action_map=[[0, 0, 0], [1], [2, 2, 2], [3, 3]]
+        wrapped_env=env,
+        action_map=[[0, 0, 0], [1], [2, 2, 2], [3, 3]],
+        reset_history=False,
     )
     h = tools.such.helper
     with h.assertRaises(AssertionError):
-        CompoundActionSequenceEnv(wrapped_env=mdp, action_map=[])
+        CompoundActionSequenceEnv(wrapped_env=env, action_map=[])
     with h.assertRaises(AssertionError):
-        CompoundActionSequenceEnv(wrapped_env=mdp, action_map=[[0, 0], [0], [1], [2]])
+        CompoundActionSequenceEnv(wrapped_env=env, action_map=[[0, 0], [0], [1], [2]])
     with h.assertRaises(AssertionError):
-        CompoundActionSequenceEnv(wrapped_env=mdp, action_map=[[], [0], [1], [2]])
+        CompoundActionSequenceEnv(wrapped_env=env, action_map=[[], [0], [1], [2]])
 
-    compound_mdp = CompoundActionSequenceEnv(mdp, action_map=[[0, 0, 0], [0, 1, 2], [2, 2, 2], [3, 3, 3]])
+    compound_env = CompoundActionSequenceEnv(env, action_map=[[0, 0, 0], [0, 1, 2], [2, 2, 2], [3, 3, 3]])
 
     def check_action_sequence(actions, outcomes):
-        compound_mdp.reset()
+        compound_env.reset()
         for action, outcome in zip(actions, outcomes):
-            obs = compound_mdp.step(action).observation
+            obs = compound_env.step(action).observation
             np.testing.assert_array_equal(obs, outcome)
 
     check_action_sequence([0, 1, 2], [0, 0, 4])
