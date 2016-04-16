f21f85012fbcc4ddda884d208850bb15921c9e93
Rocky Duan <dementrock@gmail.com>
2016-04-16 09:29:56 -0700

    skip mujoco tests if not installed
    Former-commit-id: ddd6ec40ce21f5d2e9d5f9e4fa2dd9f45134ec75

--- tests/envs/test_envs.py
+++ tests/envs/test_envs.py
@@ -8,19 +8,27 @@ from rllab.envs.box2d.double_pendulum_env import DoublePendulumEnv
 from rllab.envs.box2d.mountain_car_env import MountainCarEnv
 from rllab.envs.grid_world_env import GridWorldEnv
 from rllab.envs.identification_env import IdentificationEnv
-from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
-from rllab.envs.mujoco.hopper_env import HopperEnv
-from rllab.envs.mujoco.inverted_double_pendulum_env import InvertedDoublePendulumEnv
-from rllab.envs.mujoco.point_env import PointEnv
-from rllab.envs.mujoco.simple_humanoid_env import SimpleHumanoidEnv
-from rllab.envs.mujoco.swimmer_env import SwimmerEnv
-from rllab.envs.mujoco.walker2d_env import Walker2DEnv
-from rllab.envs.mujoco.gather.point_gather_env import PointGatherEnv
-from rllab.envs.mujoco.gather.swimmer_gather_env import SwimmerGatherEnv
-from rllab.envs.mujoco.gather.ant_gather_env import AntGatherEnv
-from rllab.envs.mujoco.maze.point_maze_env import PointMazeEnv
-from rllab.envs.mujoco.maze.swimmer_maze_env import SwimmerMazeEnv
-from rllab.envs.mujoco.maze.ant_maze_env import AntMazeEnv
+
+MUJOCO_ENABLED = True
+
+try:
+    import rllab.mujoco_py
+    from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
+    from rllab.envs.mujoco.hopper_env import HopperEnv
+    from rllab.envs.mujoco.inverted_double_pendulum_env import InvertedDoublePendulumEnv
+    from rllab.envs.mujoco.point_env import PointEnv
+    from rllab.envs.mujoco.simple_humanoid_env import SimpleHumanoidEnv
+    from rllab.envs.mujoco.swimmer_env import SwimmerEnv
+    from rllab.envs.mujoco.walker2d_env import Walker2DEnv
+    from rllab.envs.mujoco.gather.point_gather_env import PointGatherEnv
+    from rllab.envs.mujoco.gather.swimmer_gather_env import SwimmerGatherEnv
+    from rllab.envs.mujoco.gather.ant_gather_env import AntGatherEnv
+    from rllab.envs.mujoco.maze.point_maze_env import PointMazeEnv
+    from rllab.envs.mujoco.maze.swimmer_maze_env import SwimmerMazeEnv
+    from rllab.envs.mujoco.maze.ant_maze_env import AntMazeEnv
+except OSError:
+    print("Warning: Mujoco not installed. Skipping mujoco-related tests")
+    MUJOCO_ENABLED = False
 
 from rllab.envs.noisy_env import NoisyObservationEnv, DelayedActionEnv
 from rllab.envs.normalized_env import NormalizedEnv
@@ -33,20 +41,25 @@ simple_env_classes = [
     CartpoleSwingupEnv,
     DoublePendulumEnv,
     MountainCarEnv,
-    PointEnv,
-    Walker2DEnv,
-    SwimmerEnv,
-    SimpleHumanoidEnv,
-    InvertedDoublePendulumEnv,
-    HopperEnv,
-    HalfCheetahEnv,
-    PointGatherEnv,
-    SwimmerGatherEnv,
-    AntGatherEnv,
-    PointMazeEnv,
-    SwimmerMazeEnv,
-    AntMazeEnv,
 ]
+
+if MUJOCO_ENABLED:
+    simple_env_classes.extend([
+        PointEnv,
+        Walker2DEnv,
+        SwimmerEnv,
+        SimpleHumanoidEnv,
+        InvertedDoublePendulumEnv,
+        HopperEnv,
+        HalfCheetahEnv,
+        PointGatherEnv,
+        SwimmerGatherEnv,
+        AntGatherEnv,
+        PointMazeEnv,
+        SwimmerMazeEnv,
+        AntMazeEnv,
+    ])
+
 envs = [cls() for cls in simple_env_classes]
 envs.append(
     ProxyEnv(envs[0])
