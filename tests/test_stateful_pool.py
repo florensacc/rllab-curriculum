7604cb8d82d2327ebc9779bdb9b7e895428cac9b
Rocky Duan <dementrock@gmail.com>
2016-04-20 18:06:36 -0700

    fix pool tests
    Former-commit-id: bcdce4cb0412376699e77769bfde628eac0bf7ee

--- tests/test_stateful_pool.py
+++ tests/test_stateful_pool.py
@@ -8,13 +8,13 @@ def _worker_collect_once(_):
 
 def test_stateful_pool():
     from rllab.sampler import stateful_pool
-    stateful_pool.initialize(n_parallel=3)
-    results = stateful_pool.run_collect(_worker_collect_once, 3, show_prog_bar=False)
+    stateful_pool.singleton_pool.initialize(n_parallel=3)
+    results = stateful_pool.singleton_pool.run_collect(_worker_collect_once, 3, show_prog_bar=False)
     assert tuple(results) == ('a', 'a', 'a')
 
 
 def test_stateful_pool_over_capacity():
     from rllab.sampler import stateful_pool
-    stateful_pool.initialize(n_parallel=4)
-    results = stateful_pool.run_collect(_worker_collect_once, 3, show_prog_bar=False)
+    stateful_pool.singleton_pool.initialize(n_parallel=4)
+    results = stateful_pool.singleton_pool.run_collect(_worker_collect_once, 3, show_prog_bar=False)
     assert len(results) >= 3
