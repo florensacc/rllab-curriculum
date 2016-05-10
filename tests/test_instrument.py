e8b508cffa7a53f2056f6d9562b79d153cdcf83f
Rocky Duan <dementrock@gmail.com>
2016-05-09 22:01:46 -0700

    instrument enhancements
    Former-commit-id: f33bd8cd5c9995a04d6ed64e76724dfe67015eb9

--- tests/test_instrument.py
+++ tests/test_instrument.py
@@ -16,7 +16,7 @@ class TestClass(object):
 
 
 with such.A("instrument") as it:
-    @it.should("work")
+    @it.should
     def test_concretize():
         it.assertEqual(instrument.concretize([5]), [5])
         it.assertEqual(instrument.concretize((5,)), (5,))
@@ -29,6 +29,7 @@ with such.A("instrument") as it:
         it.assertIsInstance(instrument.concretize(modified()), TestClass)
 
 
+    @it.should
     def test_chained_call():
         fake_globals = dict(TestClass=TestClass)
         instrument.stub(fake_globals)
@@ -36,5 +37,36 @@ with such.A("instrument") as it:
         it.assertIsInstance(modified().arr[0], instrument.StubMethodCall)
         it.assertIsInstance(modified().compound_arr[0]["a"], instrument.StubMethodCall)
         it.assertEqual(instrument.concretize(modified().arr[0]), 1)
+
+
+    @it.should
+    def test_variant_generator():
+
+        vg = instrument.VariantGenerator()
+        vg.add("key1", [1, 2, 3])
+        vg.add("key2", [True, False])
+        vg.add("key3", lambda key2: [1] if key2 else [1, 2])
+        it.assertEqual(len(vg.variants()), 9)
+
+        class VG(instrument.VariantGenerator):
+
+            @instrument.variant
+            def key1(self):
+                return [1, 2, 3]
+
+            @instrument.variant
+            def key2(self):
+                yield True
+                yield False
+
+            @instrument.variant
+            def key3(self, key2):
+                if key2:
+                    yield 1
+                else:
+                    yield 1
+                    yield 2
+
+        it.assertEqual(len(VG().variants()), 9)
 
 it.createTests(globals())
