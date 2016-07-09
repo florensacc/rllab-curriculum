57c6df30fa25c2d42864b14a2495104b109ad052
Garrett Thomas <gwthomas@berkeley.edu>
2016-07-08 18:21:18 -0700

    Switched to neural network to estimate dynamics
    Former-commit-id: 5fb0a0fd0d38860ff44d00a157f503dff86277dc

--- sandbox/garrett/control/rlqr.py
+++ sandbox/garrett/control/rlqr.py
@@ -4,7 +4,11 @@ import theano
 import theano.tensor as TT
 from theano.tensor.nlinalg import matrix_inverse as inv
 
+import lasagne.layers as L
+import lasagne.nonlinearities as NL
+
 from rllab.spaces import Box
+from rllab.core.network import MLP
 from rllab.core.serializable import Serializable
 from rllab.policies.base import Policy
 from rllab.misc.overrides import overrides
@@ -29,26 +33,44 @@ def lqr(A, B, Q, R, k):
     return P, TT.dot(inv(R + TT.dot(B.T, TT.dot(P, B))), TT.dot(B.T, TT.dot(P, A)))
 
 class RecurrentLQRPolicy(Policy, Serializable):
-    def __init__(self, env_spec, Q, R, k=10):
+    def __init__(self, env_spec, Q, R, state_dim=None, physical_dim=None, recurrences=10):
         Serializable.quick_init(self, locals())
         assert isinstance(env_spec.observation_space, Box)
         assert isinstance(env_spec.action_space, Box)
+        assert state_dim or physical_dim
 
         observation_dim = env_spec.observation_space.flat_dim
         action_dim = env_spec.action_space.flat_dim
+
+        if state_dim and physical_dim:
+            assert observation_dim == state_dim + physical_dim
+        elif physical_dim:
+            state_dim = observation_dim - physical_dim
+        else:
+            physical_dim = observation_dim - state_dim
+
         assert R.shape == (action_dim, action_dim)
 
         float_t = theano.config.floatX
+        obs_var = env_spec.observation_space.new_tensor_variable('obs', 1)
 
-        self._A = util.init_weights(observation_dim, observation_dim)
-        self._B = util.init_weights(observation_dim, action_dim)
+        # self._A = util.init_weights(observation_dim, observation_dim)
+        # self._B = util.init_weights(observation_dim, action_dim)
+        self._dynamics_net = MLP(
+                output_dim=state_dim * (state_dim + action_dim),
+                hidden_sizes=[50, 100],
+                hidden_nonlinearity=NL.rectify,
+                output_nonlinearity=NL.rectify,
+                # input_var=obs_var[-physical_dim:],
+                input_shape=(physical_dim,)
+        )
         self._Q = theano.shared(Q.astype(float_t))
         self._R = theano.shared(R.astype(float_t))
-        self._k = k
-        self._P, self._K = lqr(self._A, self._B, self._Q, self._R, self._k)
+        self._state_dim = state_dim
+        self._physical_dim = physical_dim
+        self._action_dim = action_dim
+        self._recurrences = recurrences
 
-
-        obs_var = env_spec.observation_space.new_tensor_variable('obs', 1)
         action_var = self.get_action_sym(obs_var)
         self._f_actions = ext.compile_function([obs_var], action_var)
 
@@ -59,7 +81,8 @@ class RecurrentLQRPolicy(Policy, Serializable):
         return True
 
     def get_params_internal(self, **tags):
-        return [self._A, self._B]
+        # return [self._A, self._B]
+        return self._dynamics_net.get_params_internal()
 
     def get_action(self, observation):
         action = self._f_actions([observation])[0]
@@ -71,6 +94,16 @@ class RecurrentLQRPolicy(Policy, Serializable):
     def get_action_sym(self, obs_var):
         return self._forward(obs_var)
 
+    def _map_dynamics(self, obs_var):
+        physical_var = obs_var[:,self._state_dim:]
+        net_out = L.get_output(self._dynamics_net.output_layer, inputs=physical_var)[0]
+        # With regards to taking only the first row of output:                       ^
+        # It's ok because the physical parameters will not change mid-episode
+        
+        A = net_out[:self._state_dim**2].reshape((self._state_dim, self._state_dim))
+        B = net_out[self._state_dim**2:].reshape((self._state_dim, self._action_dim))
+        return A, B
+
     def _forward(self, obs_var):
         # u = TT.dot(W, x)
         # for i in range(k):
@@ -78,4 +111,7 @@ class RecurrentLQRPolicy(Policy, Serializable):
         #     tmp2 = TT.dot(B.T, TT.dot(P, TT.dot(A, x)))
         #     u = -TT.dot(inv(R), tmp1+tmp2)
 
-        return -TT.dot(self._K, obs_var.T).T
+        self._A, self._B = self._map_dynamics(obs_var)
+        self._P, self._K = lqr(self._A, self._B, self._Q, self._R, self._recurrences)
+        state_var = obs_var[:,:self._state_dim]
+        return -TT.dot(self._K, state_var.T).T
