a97a9d5f5a7aa77cd0963d5640635bdc1a946d21
Xi Chen <adslcx@gmail.com>
2016-08-07 00:09:11 -0700

    no ar exp

--- sandbox/pchen/InfoGAN/launchers/res_ar_isre_iaf_init_sweep.py
+++ sandbox/pchen/InfoGAN/launchers/res_ar_isre_iaf_init_sweep.py
@@ -62,7 +62,8 @@ class VG(VariantGenerator):
         # return [0,]#2,4]
         # return [2,]#2,4]
         # return [0,1,]#4]
-        return [4,]
+        # return [4,]
+        return [0]
 
     @variant
     def nr(self, nar):
