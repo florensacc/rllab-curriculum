3ad23e8ad625a51e779073f5b8a635b3b4c6ca2d
hrtang <hrtang.alex@gmail.com>
2016-09-06 22:47:55 -0700

    more convenient ec2 pricing

--- sandbox/haoran/tf/distributions/recurrent_diagonal_gaussian.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-from __future__ import absolute_import
-
-from sandbox.rocky.tf.distributions.diagonal_gaussian import DiagonalGaussian
-
-RecurrentDiagonalGaussian = DiagonalGaussian
