9486c4fce5185a4e2f4e5671c0f428511f2e5ad5
Xi Chen <adslcx@gmail.com>
2016-09-09 14:56:07 -0700

    cifar exp

--- sandbox/pchen/InfoGAN/infogan/models/regularized_helmholtz_machine.py
+++ sandbox/pchen/InfoGAN/infogan/models/regularized_helmholtz_machine.py
@@ -1579,6 +1579,87 @@ class RegularizedHelmholtzMachine(object):
                          # dropout(0.9).
                          flatten().
                          fully_connected(self.reg_latent_dist.dist_flat_dim, activation_fn=None))
+            elif self.network_type == "big_resv1_k3_pixel_bias_cifar_pred_scale":
+                from prettytensor import UnboundVariable
+                with pt.defaults_scope(
+                        activation_fn=tf.nn.elu,
+                        custom_phase=UnboundVariable('custom_phase'),
+                        wnorm=self.wnorm,
+                        pixel_bias=True,
+                ):
+                    encoder = \
+                        (pt.template('input', self.book).
+                         reshape([-1] + list(image_shape))
+                         )
+                    from sandbox.pchen.InfoGAN.infogan.misc.custom_ops import resconv_v1, resdeconv_v1
+                    encoder = resconv_v1(encoder, 5, 16, stride=2) #16
+                    encoder = resconv_v1(encoder, 5, 16, stride=1)
+                    encoder = resconv_v1(encoder, 5, 32, stride=2) #8
+                    encoder = resconv_v1(encoder, 5, 32, stride=1)
+                    encoder = resconv_v1(encoder, 3, 64, stride=2) #4
+                    encoder = resconv_v1(encoder, 3, 64, stride=1)
+                    self.encoder_template = \
+                        (encoder.
+                         flatten().
+                         wnorm_fc(800, ).
+                         wnorm_fc(self.inference_dist.dist_flat_dim, activation_fn=None)
+                         )
+                    decoder = (pt.template('input', self.book).
+                               # wnorm_fc(450, ).
+                               wnorm_fc(1024, ).
+                               reshape([-1, 4, 4, 64])
+                               )
+                    decoder = resconv_v1(decoder, 3, 64, stride=1)
+                    decoder = resdeconv_v1(decoder, 3, 32, out_wh=[8,8])
+                    decoder = resconv_v1(decoder, 5, 32, stride=1)
+                    decoder = resdeconv_v1(decoder, 5, 32, out_wh=[16,16])
+                    decoder = resconv_v1(decoder, 5, 32, stride=1)
+                    decoder = resdeconv_v1(decoder, 5, 16, out_wh=[32,32])
+                    decoder = resconv_v1(decoder, 5, 16, stride=1)
+                    if isinstance(self.output_dist, Mixture):
+                        modes = self.output_dist.modes
+                    else:
+                        modes = 1
+                    # modes have diff scale params
+                    # scale_var = tf.Variable(
+                    #     initial_value=np.zeros([1,modes,3,1,1], dtype='float32'),
+                    #     name="channel_scale"
+                    # )
+                    self.decoder_template = (
+                        decoder.
+                            conv2d_mod(
+                            5,
+                            3*modes*2,
+                            activation_fn=None
+                        ).
+                            apply(tf.transpose, [0, 3, 1, 2]).
+                            reshape([-1, modes, 3*2, 32, 32]).
+                            # apply(lambda conv:
+                            #       tf.concat(
+                            #           2,
+                            #           [
+                            #               tf.clip_by_value(
+                            #                   conv,
+                            #                   -0.5 + 1 / 512.,
+                            #                   0.5 - 1 / 512.
+                            #               ),
+                            #               conv*0. + scale_var
+                            #           ]
+                            #       )
+                            #       ).
+                            reshape([-1, modes, 2, 3, 32, 32]).
+                            apply(tf.transpose, [0, 1, 2, 4, 5, 3]).
+                            flatten()
+                    )
+                    self.reg_encoder_template = \
+                        (pt.template('input').
+                         reshape([self.batch_size] + list(image_shape)).
+                         custom_conv2d(5, 32, ).
+                         custom_conv2d(5, 64, ).
+                         custom_conv2d(5, 128, edges='VALID').
+                         # dropout(0.9).
+                         flatten().
+                         fully_connected(self.reg_latent_dist.dist_flat_dim, activation_fn=None))
             elif self.network_type == "small_conv":
                 from prettytensor import UnboundVariable
                 keep_prob = network_args["keep_prob"]
