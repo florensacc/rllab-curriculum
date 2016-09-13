72e6421cd9941bdd2c273314acd1de9c7342be46
Xi Chen <adslcx@gmail.com>
2016-09-13 01:20:11 -0700

    stage simple arconv work

---
+++ sandbox/pchen/InfoGAN/launchers/0912/simpe_convar_mnist.py
@@ -0,0 +1,6 @@
+import os
+from sandbox.pchen.InfoGAN.infogan.misc.datasets import MnistDataset, FaceDataset, BinarizedMnistDataset, \
+    ResamplingBinarizedMnistDataset
+import numpy as np
+import tensorflow as tf
+
