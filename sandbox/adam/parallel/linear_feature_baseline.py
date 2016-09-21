19be58a3140876edaf52de25fe95a4b3f9b582ef
Adam Stooke <adam.stooke@gmail.com>
2016-09-21 13:14:22 -0700

    baseline fix

--- sandbox/adam/parallel/linear_feature_baseline.py
+++ sandbox/adam/parallel/linear_feature_baseline.py
@@ -1,35 +1,28 @@
-from rllab.baselines.base import Baseline
-from rllab.misc.overrides import overrides
+
 import numpy as np
 import multiprocessing as mp
+
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.misc.overrides import overrides
+
 from sandbox.adam.parallel.util import SimpleContainer
 
 
-class ParallelLinearFeatureBaseline(Baseline):
+class ParallelLinearFeatureBaseline(LinearFeatureBaseline):
 
-    def __init__(self, env_spec, reg_coeff=1e-5):
-        self._coeffs = None
-        self._reg_coeff = reg_coeff
+    def __init__(self, env_spec, reg_coeff=1e-5, low_mem=False):
         self._vec_dim = 2 * int(env_spec.observation_space.flat_dim) + 3 + 1
-        self._par_data = None  # objects for parallelism, to be populated
-        self._shareds = None
-        self._mgr_objs = None
+        self._low_mem = low_mem
+        super().__init__(env_spec, reg_coeff)
 
     def __getstate__(self):
+        """ Do not try to serialize parallel objects."""
         return {k: v for k, v in iter(self.__dict__.items()) if k != "_par_objs"}
 
     @overrides
     @property
     def algorithm_parallelized(self):
         return True
-
-    @overrides
-    def get_param_values(self, **tags):
-        return self._coeffs
-
-    @overrides
-    def set_param_values(self, val, **tags):
-        self._coeffs = val
 
     def init_par_objs(self, n_parallel):
         """
@@ -38,50 +31,21 @@ class ParallelLinearFeatureBaseline(Baseline):
         subprocess--neeeded in Windows.)
         """
         par_data = SimpleContainer(rank=None)
-
         shareds = SimpleContainer(
             feat_mat=np.reshape(
                 np.frombuffer(mp.RawArray('d', self._vec_dim ** 2)),
                 (self._vec_dim, self._vec_dim)),
             target_vec=np.frombuffer(mp.RawArray('d', self._vec_dim)),
         )
-
         mgr_objs = SimpleContainer(
             lock=mp.Lock(),
             barriers_fit=[mp.Barrier(n_parallel) for _ in range(2)],
         )
-
         self._par_objs = (par_data, shareds, mgr_objs)
 
     def set_rank(self, rank):
         par_data, _, _ = self._par_objs
         par_data.rank = rank
-
-    def _all_features_and_targets(self, paths):
-        """
-        Sum path data (in matrix form) rather than concatenate, to generate a
-        small set of values for fitting.
-        """
-        feat_mat = np.zeros([self._vec_dim, self._vec_dim])
-        target_vec = np.zeros([self._vec_dim, ])
-        path_vec = np.zeros([1, self._vec_dim])
-        max_len = max([len(path["rewards"]) for path in paths])
-        times = np.arange(max_len).reshape(-1, 1) / 100.0
-        times_2 = times ** 2
-        times_3 = times ** 3
-        for path in paths:
-            obs = np.clip(path["observations"], -10, 10)
-            for o, al, al_2, al_3, ret in zip(obs, times, times_2, times_3, path["returns"]):
-                path_vec[:] = np.concatenate([o, o ** 2, al, al_2, al_3, [1.]])
-                feat_mat += path_vec.T.dot(path_vec)
-                target_vec += path_vec.squeeze() * ret
-        return feat_mat, target_vec
-
-    def _features(self, path):
-        o = np.clip(path["observations"], -10, 10)
-        h = len(path["rewards"])
-        al = np.arange(h).reshape(-1, 1) / 100.0
-        return np.concatenate([o, o ** 2, al, al ** 2, al ** 3, np.ones((h, 1))], axis=1)
 
     @overrides
     def fit(self, paths):
@@ -90,7 +54,14 @@ class ParallelLinearFeatureBaseline(Baseline):
         """
         par_data, shareds, mgr_objs = self._par_objs
 
-        feat_mat, target_vec = self._all_features_and_targets(paths)
+        if self._low_mem:
+            feat_mat, target_vec = self._features_and_targets(paths)
+        else:
+            featmat = np.concatenate([self._features(path) for path in paths])
+            returns = np.concatenate([path["returns"] for path in paths])
+            feat_mat = featmat.T.dot(featmat)
+            target_vec = featmat.T.dot(returns)
+
         if par_data.rank == 0:
             shareds.feat_mat[:] = feat_mat
             shareds.target_vec[:] = target_vec
@@ -107,14 +78,28 @@ class ParallelLinearFeatureBaseline(Baseline):
             # NOTE: Could parallelize this loop, but fitting is usually fast.
             self._coeffs = np.linalg.lstsq(
                 shareds.feat_mat + reg_coeff * np.identity(feat_mat.shape[1]),
-                target_vec
+                shareds.target_vec
             )[0]
             if not np.any(np.isnan(self._coeffs)):
                 break
             reg_coeff *= 10
 
-    @overrides
-    def predict(self, path):
-        if self._coeffs is None:
-            return np.zeros(len(path["rewards"]))
-        return self._features(path).dot(self._coeffs)
+    def _features_and_targets(self, paths):
+        """
+        Optional: sum path data (in matrix form) rather than concatenate, to
+        keep memory footprint smaller.
+        """
+        feat_mat = np.zeros([self._vec_dim, self._vec_dim])
+        target_vec = np.zeros([self._vec_dim, ])
+        path_vec = np.zeros([1, self._vec_dim])
+        max_len = max([len(path["rewards"]) for path in paths])
+        t = np.arange(max_len).reshape(-1, 1) / 100.0
+        t2 = t ** 2
+        t3 = t ** 3
+        for path in paths:
+            obs = np.clip(path["observations"], -10, 10)
+            for o, al, al2, al3, ret in zip(obs, t, t2, t3, path["returns"]):
+                path_vec[:] = np.concatenate([o, o ** 2, al, al2, al3, [1.]])
+                feat_mat += path_vec.T.dot(path_vec)
+                target_vec += path_vec.squeeze() * ret
+        return feat_mat, target_vec
