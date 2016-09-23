9341256bfe1250b033f7b9352e840169531142c0
Xi Chen <adslcx@gmail.com>
2016-09-23 01:26:52 -0700

    basic ensemble & information leakage

--- sandbox/pchen/InfoGAN/launchers/0921/run_mnist_egan.py
+++ sandbox/pchen/InfoGAN/launchers/0921/run_mnist_egan.py
@@ -1,9 +1,11 @@
+from sandbox.pchen.InfoGAN.infogan.algos.ensemble_gan_trainer import EnsembleGANTrainer
 from sandbox.pchen.InfoGAN.infogan.algos.gan_trainer import GANTrainer
 from sandbox.pchen.InfoGAN.infogan.misc.distributions import Uniform, Categorical, Gaussian, MeanBernoulli
 
 import tensorflow as tf
 import os
 from sandbox.pchen.InfoGAN.infogan.misc.datasets import MnistDataset
+from sandbox.pchen.InfoGAN.infogan.models.ensemble_gan import EnsembleGAN
 from sandbox.pchen.InfoGAN.infogan.models.gan import GAN
 from sandbox.pchen.InfoGAN.infogan.models.regularized_gan import RegularizedGAN
 from sandbox.pchen.InfoGAN.infogan.algos.infogan_trainer import InfoGANTrainer
@@ -23,8 +25,9 @@ if __name__ == "__main__":
     updates_per_epoch = 100
     max_epoch = 100
 
-    exp_name = "logit_mnist_%s" % timestamp
 
+    nr_model = 2
+    exp_name = "real_dps_dls_single_%sensemble_mnist_%s" % (nr_model, timestamp)
     log_dir = os.path.join(root_log_dir, exp_name)
     checkpoint_dir = os.path.join(root_checkpoint_dir, exp_name)
 
@@ -40,28 +43,31 @@ if __name__ == "__main__":
         # (Uniform(1, ), True),
     ]
 
-    for glr in [5e-4, 1e-4]:
-        model = GAN(
-            output_dist=MeanBernoulli(dataset.image_dim),
-            latent_spec=latent_spec,
-            batch_size=batch_size,
-            image_shape=dataset.image_shape,
-            network_type="mnist",
-        )
+    glr = 5e-3
+    model = EnsembleGAN(
+        output_dist=MeanBernoulli(dataset.image_dim),
+        latent_spec=latent_spec,
+        batch_size=batch_size,
+        image_shape=dataset.image_shape,
+        network_type="mnist",
+        nr_models=nr_model,
+    )
 
-        algo = GANTrainer(
-            model=model,
-            dataset=dataset,
-            batch_size=batch_size,
-            exp_name=("glr_%s"%glr)+exp_name,
-            log_dir=log_dir,
-            checkpoint_dir=checkpoint_dir,
-            max_epoch=max_epoch,
-            updates_per_epoch=updates_per_epoch,
-            # info_reg_coeff=1.0,
-            generator_learning_rate=glr,
-            discriminator_learning_rate=2e-4,
-        )
+    algo = EnsembleGANTrainer(
+        model=model,
+        dataset=dataset,
+        batch_size=batch_size,
+        exp_name=("glr_%s"%glr)+exp_name,
+        log_dir=log_dir,
+        checkpoint_dir=checkpoint_dir,
+        max_epoch=max_epoch,
+        updates_per_epoch=updates_per_epoch,
+        # info_reg_coeff=1.0,
+        generator_learning_rate=glr,
+        discriminator_learning_rate=2e-4,
+        discriminator_leakage="single",
+        discriminator_priviledge="single",
+    )
 
-        algo.train()
+    algo.train()
 
