506dea43d23d25d37e0b001648a84a25d09ee8fa
Carlos Florensa <florensacc@berkeley.edu>
2016-11-14 14:58:09 -0800

    put ec2_info in config_personal

--- scripts/setup_ec2_for_rllab.py
+++ scripts/setup_ec2_for_rllab.py
@@ -300,7 +300,13 @@ def setup_ec2():
             key_pair = ec2_client.create_key_pair(KeyName=key_name)
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == 'InvalidKeyPair.Duplicate':
-                if not query_yes_no("Key pair with name %s exists. Proceed to delete and recreate?" % key_name, "no"):
+                response = query_yes_no("Key pair with name %s exists. Proceed to delete and recreate?" % key_name,
+                                        "no", allow_skip=True)
+                if response == 'skip':
+                    print("Keeping your old key...")
+                    pass
+                elif not response:
+                    print("Exiting...")
                     sys.exit()
                 print("Deleting existing key pair with name %s" % key_name)
                 ec2_client.delete_key_pair(KeyName=key_name)
