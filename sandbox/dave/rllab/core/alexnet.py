65b74438e7584c0975706ded15070228303228f8
Ignasi <ignasi78@gmail.com>
2016-11-15 16:52:04 -0800

    AlexNet modifications+baseline

--- sandbox/dave/rllab/core/alexnet.py
+++ sandbox/dave/rllab/core/alexnet.py
@@ -1,10 +1,11 @@
 # from lasagne.layers.dnn import Conv2DDNNLayer as Conv2DLayer
 from lasagne.layers import MaxPool2DLayer, LocalResponseNormalization2DLayer, Conv2DLayer
-from lasagne.layers import SliceLayer, concat, DenseLayer
+from lasagne.layers import SliceLayer, concat, DenseLayer, ReshapeLayer
 import lasagne.nonlinearities
 import lasagne
 import lasagne.init as LI
 import theano
+from sandbox.dave.rllab.core.lasagne_layers import FlattenLayer
 
 # Caffe reference model lasagne implementation
 # http://caffe.berkeleyvision.org/
@@ -48,7 +49,6 @@ class AlexNet(object):
             convolution=wrapped_conv,
         )
 
-        # pool1
         net['pool1'] = MaxPool2DLayer(net['conv1'], pool_size=(3, 3), stride=2, name='pool1')
 
         # norm1
@@ -146,6 +146,7 @@ class AlexNet(object):
                                          num_filters=128,
                                          filter_size=(3, 3),
                                          pad=1,
+
                                          name='conv5_part1',
                                          convolution=wrapped_conv,
                                          )
@@ -157,40 +158,48 @@ class AlexNet(object):
                                          convolution=wrapped_conv,
                                          )
         net['conv5'] = concat((net['conv5_part1'], net['conv5_part2']), axis=1, name='conv5')
+        net['output'] = FlattenLayer(net['conv5'])
 
         # pool 5
-        net['pool5'] = MaxPool2DLayer(net['conv5'], pool_size=(3, 3), stride=2, name='pool5')
+        # net['pool5'] = MaxPool2DLayer(net['conv5'], pool_size=(3, 3), stride=2, name='pool5')
 
         # fc6
-        net['fc6'] = DenseLayer(
-            net['pool5'], num_units=4096,
-            nonlinearity=lasagne.nonlinearities.rectify,
-            name='fc6',
-            W=LI.GlorotUniform(),
-            b=LI.Constant(0.))
+        #   net['fc6'] = DenseLayer(
+        #     net['pool5'], num_units=4096,
+        #     nonlinearity=lasagne.nonlinearities.rectify,
+        #     name='fc6',
+        #     W=LI.GlorotUniform(),
+        #     b=LI.Constant(0.))
+        #
+        # # fc7
+        # net['fc7'] = DenseLayer(
+        #     net['fc6'],
+        #     num_units=4096,
+        #     nonlinearity=lasagne.nonlinearities.rectify,
+        #     name='fc7',
+        #     W=LI.GlorotUniform(),
+        #     b=LI.Constant(0.))
+        #
+        # # fc8
+        # net['fc8'] = DenseLayer(
+        #     net['fc7'],
+        #     num_units=1000,
+        #     nonlinearity=lasagne.nonlinearities.softmax,
+        #     name='fc8',
+        #     W=LI.GlorotUniform(),
+        #     b=LI.Constant(0.))
 
-        # fc7
-        net['fc7'] = DenseLayer(
-            net['fc6'],
-            num_units=4096,
-            nonlinearity=lasagne.nonlinearities.rectify,
-            name='fc7',
-            W=LI.GlorotUniform(),
-            b=LI.Constant(0.))
-
-        # fc8
-        net['fc8'] = DenseLayer(
-            net['fc7'],
-            num_units=1000,
-            nonlinearity=lasagne.nonlinearities.softmax,
-            name='fc8',
-            W=LI.GlorotUniform(),
-            b=LI.Constant(0.))
+        # net['fc8'] = DenseLayer(
+        #     net['pool5'], num_units=96,
+        #     nonlinearity=lasagne.nonlinearities.rectify,
+        #     name='fc8',
+        #     W=LI.GlorotUniform(),
+        #     b=LI.Constant(0.))
 
         self._l_in = net['data']
         self._layers = net
-        self._l_out = net['fc8']
-        self._conv_out = net['pool5']
+        self._l_out = net['output']
+        self._conv_out = net['output']
 
     @property
     def input_layer(self):
@@ -216,27 +225,75 @@ class AlexNet(object):
 class VanillaConvNet(object):
     def __init__(self, input_layer=None):
         net = {}
+        # if input_layer is None:
         net['data'] = input_layer
+        # else:
+            # net['data'] = input_layer
+
+        # conv1
         net['conv1'] = Conv2DLayer(
-                net['data'],
-                num_filters=20,
-                filter_size=(4, 4),
-                stride=2,
-                name='conv1',
-                convolution=wrapped_conv,
-            )
-        net['fc8'] = DenseLayer(
-            net['conv1'],
-            num_units=100,
-            nonlinearity=lasagne.nonlinearities.softmax,
-            name='fc8',
+            net['data'],
+            num_filters=96,
+            filter_size=(11, 11),
+            stride=4,
+            name='conv1',
+            convolution=wrapped_conv,
+        )
+
+        net['pool1'] = MaxPool2DLayer(net['conv1'], pool_size=(3, 3), stride=2, name='pool1')
+
+        # norm1
+        net['norm1'] = LocalResponseNormalization2DLayer(net['pool1'],
+                                                         n=5,
+                                                         alpha=0.0001 / 5.0,
+                                                         beta=0.75,
+                                                         k=1,
+                                                         name='norm1',
+                                                         )
+
+        net['conv2_data1'] = SliceLayer(net['norm1'], indices=slice(0, 48), axis=1, name='conv2_data1')
+        net['conv2_data2'] = SliceLayer(net['norm1'], indices=slice(48, 96), axis=1, name='conv2_data2')
+
+        # now do the convolutions
+        net['conv2_part1'] = Conv2DLayer(net['conv2_data1'],
+                                         num_filters=128,
+                                         filter_size=(5, 5),
+                                         pad=2,
+                                         name='conv2_part1',
+                                         convolution=wrapped_conv,
+                                         )
+        net['conv2_part2'] = Conv2DLayer(net['conv2_data2'],
+                                         num_filters=128,
+                                         filter_size=(5, 5),
+                                         pad=2,
+                                         name='conv2_part2',
+                                         convolution=wrapped_conv,
+                                         )
+
+        # now combine
+        net['conv2'] = concat((net['conv2_part1'], net['conv2_part2']), axis=1, name='conv2')
+
+        # pool2
+        net['pool2'] = MaxPool2DLayer(net['conv2'], pool_size=(3, 3), stride=2, name='pool2')
+
+        net['fc3'] = DenseLayer(
+            net['pool2'], num_units=64,
+            nonlinearity=lasagne.nonlinearities.rectify,
+            name='fc2',
             W=LI.GlorotUniform(),
             b=LI.Constant(0.))
+        #
+        # net['fc4'] = DenseLayer(
+        #     net['fc3'], num_units=10,
+        #     nonlinearity=lasagne.nonlinearities.rectify,
+        #     name='fc4',
+        #     W=LI.GlorotUniform(),
+        #     b=LI.Constant(0.))
 
         self._l_in = net['data']
         self._layers = net
-        self._l_out = net['fc8']
-        self._conv_out = net['conv1']
+        self._l_out = net['fc3']
+        self._conv_out = net['pool2']
 
     @property
     def input_layer(self):
