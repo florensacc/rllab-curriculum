a2b94fd3476dfcebdf3d0de760a5cb000964e400
Sandy Huang <shhuang@cs.berkeley.edu>
2017-01-20 20:41:49 -0800

    Running A3C with Haoran's AtariEnv -- sanity check

--- sandbox/sandy/async_rl/algos/a3c_ale.py
+++ sandbox/sandy/async_rl/algos/a3c_ale.py
@@ -3,6 +3,7 @@ from rllab.misc.overrides import overrides
 
 class A3CALE(AsyncAlgo):
     def set_process_params(self,process_id,env,agent,training_args):
+        self.process_id = process_id
         agent.process_id = process_id
 
     def get_snapshot(self,env,agent):
