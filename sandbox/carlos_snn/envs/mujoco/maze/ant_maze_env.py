0ccf73353f9faf7bea703aad59c2acbd07cdde93
florensacc <florensacc@berkeley.edu>
2017-01-29 23:42:10 -0800

    refactor Maze and gather

--- sandbox/carlos_snn/envs/mujoco/maze/ant_maze_env.py
+++ sandbox/carlos_snn/envs/mujoco/maze/ant_maze_env.py
@@ -1,23 +1,18 @@
-# from sandbox.carlos_snn.envs.mujoco.maze.maze_env import MazeEnv
-from sandbox.carlos_snn.envs.mujoco.maze.fast_maze_env import FastMazeEnv  # %^&*&^%
+from sandbox.carlos_snn.envs.mujoco.maze.fast_maze_env import FastMazeEnv
 from sandbox.carlos_snn.envs.mujoco.ant_env import AntEnv
 from rllab.misc.overrides import overrides
-
-from rllab.envs.normalized_env import normalize
-from rllab.core.serializable import Serializable
 
 from rllab.envs.mujoco.mujoco_env import q_mult, q_inv
 import math
 
 
-class AntMazeEnv(FastMazeEnv, Serializable):
+class AntMazeEnv(FastMazeEnv):
 
     MODEL_CLASS = AntEnv
     ORI_IND = 3
 
     MAZE_HEIGHT = 3
     MAZE_SIZE_SCALING = 3.0
-    # MAZE_MAKE_CONTACTS = True
 
     @overrides
     def get_ori(self):
