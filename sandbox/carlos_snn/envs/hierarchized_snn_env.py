ccc0ebc210a3a3de52cac5569d4d1578cc0606de
florensacc <florensacc@berkeley.edu>
2017-02-01 00:03:30 -0800

    hierarchizedEnvs

--- sandbox/carlos_snn/envs/hierarchized_snn_env.py
+++ sandbox/carlos_snn/envs/hierarchized_snn_env.py
@@ -1,8 +1,7 @@
 import json
-import os
-
 import joblib
 import numpy as np
+import os
 
 from rllab import config
 from rllab import spaces
@@ -14,7 +13,7 @@ from rllab.misc import tensor_utils
 from rllab.misc.overrides import overrides
 from sandbox.carlos_snn.envs.mujoco.maze.fast_maze_env import FastMazeEnv
 from sandbox.carlos_snn.policies.hier_snn_mlp_policy import GaussianMLPPolicy_snn_hier
-from sandbox.carlos_snn.sampler.utils import rollout
+from sandbox.carlos_snn.sampler.utils import rollout  # this is a different rollout (option of no reset)
 
 
 class HierarchizedSnnEnv(ProxyEnv, Serializable):
@@ -29,6 +28,15 @@ class HierarchizedSnnEnv(ProxyEnv, Serializable):
             animate=False,
             keep_rendered_rgb=False,
     ):
+        """
+        :param env: Env to wrap, should have same robot characteristics than env where the policy where pre-trained on
+        :param time_steps_agg: Time-steps during which the SNN policy is executed with fixed (discrete) latent
+        :param discrete_actions: whether the policy are applied alone or with a linear combination
+        :param pkl_path: path to pickled pre-training experiment that includes the pre-trained policy
+        :param json_path: path to json of the pre-training experiment. Requires npz_paths of the policy params
+        :param npz_path: only required when using json_path
+        :param keep_rendered_rgb: the returned frac_paths include all rgb images (for plotting video after)
+        """
         Serializable.quick_init(self, locals())
         ProxyEnv.__init__(self, env)
         self.time_steps_agg = time_steps_agg
@@ -45,7 +53,6 @@ class HierarchizedSnnEnv(ProxyEnv, Serializable):
         else:
             raise Exception("No path to file given")
 
-        # I need to define a new hier-policy that will cope with that!
         self.low_policy = GaussianMLPPolicy_snn_hier(
             env_spec=env.spec,
             env=env,
@@ -90,26 +97,20 @@ class HierarchizedSnnEnv(ProxyEnv, Serializable):
                 next_obs = frac_path['observations'][-1]
 
             reward = np.sum(frac_path['rewards'])
+            terminated = frac_path['terminated'][-1]
             done = self.time_steps_agg > len(
-                frac_path['observations'])  # if the rollout was not maximal it was "done"!`
+                frac_path['observations']) or terminated  # if the rollout was not maximal it was "done"
             # it would be better to add an extra flagg to this rollout to check if it was done in the last step
             last_agent_info = dict((k, val[-1]) for k, val in frac_path['agent_infos'].items())
             last_env_info = dict((k, val[-1]) for k, val in frac_path['env_infos'].items())
         # print("finished step of {}, with cummulated reward of: {}".format(len(frac_path['observations']), reward))
-        # print("Next obs (com): {}, rew: {}, last_env_info: {}, last_agent_info: {}".format(last_env_info, reward, last_env_info,
-        #                                                                              last_agent_info))
         if done:
-            print("\n ########## \n ***** done!! *****")
-            # if done I need to PAD the tensor so there is no mismatch! Pad with what? with the last elem!
-            # I need to pad first the env_infos!!
+            # if done I need to PAD the tensor so there is no mismatch. Pad with the last elem, but not the env_infos!
             frac_path['env_infos'] = tensor_utils.pad_tensor_dict(frac_path['env_infos'], self.time_steps_agg)
             full_path = tensor_utils.pad_tensor_dict(frac_path, self.time_steps_agg, mode='last')
-            # you might be padding the rewards!!! Error!!!
+            # you might be padding the rewards
             actual_path_length = len(frac_path['rewards'])
             full_path['rewards'][actual_path_length:] = 0.
-            # do the same for the maze_rewards
-            if 'env_infos' in full_path.keys() and 'maze_rewards' in full_path['env_infos']:
-                full_path['env_infos']['maze_rewards'][actual_path_length:] = 0.
         else:
             full_path = frac_path
 
@@ -119,8 +120,7 @@ class HierarchizedSnnEnv(ProxyEnv, Serializable):
 
     @overrides
     def log_diagnostics(self, paths, *args, **kwargs):
-        ## to use the visualization I need to append all paths!
-        ## and also I need the paths to have the "agent_infos" key including the latent!!
+        # to use the visualization I need to append all paths
         expanded_paths = [tensor_utils.flatten_first_axis_tensor_dict(path['env_infos']['full_path']) for path in paths]
         self.wrapped_env.log_diagnostics(expanded_paths, *args, **kwargs)
 
