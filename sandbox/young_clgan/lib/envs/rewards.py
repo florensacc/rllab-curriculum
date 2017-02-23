0c39b49f18ab9fcd48195c3ede3a324f23d2d532
florensacc <florensacc@berkeley.edu>
2017-02-22 20:17:57 -0800

    comments

--- sandbox/young_clgan/lib/envs/rewards.py
+++ sandbox/young_clgan/lib/envs/rewards.py
@@ -4,7 +4,12 @@ import numpy as np
 """ Reward function utilities."""
 
 def linear_threshold_reward(distance, threshold, coefficient):
-    """Linear reward with respect to distance, cut of at threshold."""
+    """
+    Linear reward with respect to distance, cut of at threshold.
+    :param distance: current distance to the goal
+    :param threshold: maximum distance at which some bonus is given
+    :param coefficient: NEGATIVE --> slope of the linear bonus
+    """
     assert distance >= 0 and threshold > 0 and coefficient < 0
     constant = -threshold * coefficient
     return max(distance * coefficient + constant, 0)
