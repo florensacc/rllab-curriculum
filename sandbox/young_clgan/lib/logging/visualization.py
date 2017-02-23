6beda4a23f18d7fe13ca12effeafe378dd79250b
florensacc <florensacc@berkeley.edu>
2017-02-22 20:19:56 -0800

    add 3d plotting

--- sandbox/young_clgan/lib/logging/visualization.py
+++ sandbox/young_clgan/lib/logging/visualization.py
@@ -10,6 +10,7 @@ from sandbox.young_clgan.lib.envs.base import FixedGoalGenerator
 import matplotlib
 
 matplotlib.use('Agg')
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 
 
@@ -56,40 +57,68 @@ def plot_policy_reward(policy, env, limit, horizon=200, max_reward=6000, fname=N
             return img
 
 
-def plot_labeled_samples(samples, sample_classes=None, text_labels=None, fname=None, limit=None,
-                         labels=None, size=1000, colors=['red', 'green', 'blue', 'yellow']):
-    if sample_classes is None and text_labels is None:
-        sample_classes, text_labels = convert_label(labels)
+def plot_labeled_samples(samples, sample_classes=None, text_labels=None, markers=None, fname=None, limit=None,
+                         size=1000, colors=('k', 'm', 'c', 'y', 'r', 'g', 'b')):
     size = min(size, samples.shape[0])
     indices = np.random.choice(samples.shape[0], size, replace=False)
     samples = samples[indices, :]
     sample_classes = sample_classes[indices]
-
-    # point_class = labels[:, 0] * 2 + labels[:, 1]
-    plt.clf()
-    # text_labels = ['', 'Reward not sensible', 'Goal already accomplished', 'Desired goals']
-
-    # colors = ['b', 'g', 'r', 'y', 'c', 'm']
-    colors = ['k', 'm', 'c', 'y', 'r', 'g', 'b']
+    if markers is None:
+        markers = {i:'o' for i in text_labels.keys()}
 
     unique_classes = list(set(sample_classes))
-
     assert (len(colors) > max(unique_classes))
 
-    for i in unique_classes:
-        plt.scatter(
-            samples[sample_classes == i, 0],
-            samples[sample_classes == i, 1],
-            # Choose a fixed color for each class.
-            c=colors[i],
-            alpha=0.8,
-            label=text_labels[i]
-        )
+    fig = plt.figure()
+    if len(samples[0]) >= 3:
+        ax = fig.add_subplot(111, projection='3d')
+        for i in unique_classes:
+            ax.scatter(
+                samples[sample_classes == i, 0],
+                samples[sample_classes == i, 1],
+                samples[sample_classes == i, 2],
+                # Choose a fixed color for each class.
+                c=colors[i],
+                marker=markers[i],
+                alpha=0.8,
+                label=text_labels[i]
+            )
+        ax.set_ylim3d(-limit, limit)
+        ax.set_xlim3d(-limit, limit)
+        ax.set_zlim3d(-limit, limit)
+            # high, low = self.wrapped_env.observation_space.bounds
+            # for i in range(3):
+            #     j = (i + 1) % 3
+            #     k = (i + 2) % 3
+            #     a = high[idx]
+            #     a[i] = low[idx[i]]
+            #     b = low[idx]
+            #     b[j] = high[idx[j]]
+            #     c = low[idx]
+            #     c[k] = high[idx[k]]
+            #     # import pdb; pdb.set_trace()
+            #     print("segments are: ", *zip(high[idx], b), 'and ', *zip(high[idx], b), 'and ', *zip(a, b), ' and ', *zip(a, c))
+            #     ax.plot(*zip(high[idx], a), color="b")
+            #     ax.plot(*zip(low[idx], b), color="b")
+            #     ax.plot(*zip(a, b), color='b')
+            #     ax.plot(*zip(a, c), color='b')
+    else:
+        for i in unique_classes:
+            plt.scatter(
+                samples[sample_classes == i, 0],
+                samples[sample_classes == i, 1],
+                # samples[sample_classes == i, 2],
+                # Choose a fixed color for each class.
+                c=colors[i],
+                alpha=0.8,
+                label=text_labels[i]
+            )
 
-    plt.ylim(-limit, limit)
-    plt.xlim(-limit, limit)
+        plt.ylim(-limit, limit)
+        plt.xlim(-limit, limit)
     # Place the legend to the right of the plot.
     lgd = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
     if fname is not None:
         plt.savefig(fname, format='png', bbox_extra_artists=(lgd,), bbox_inches='tight')
         return scipy.misc.imread(fname)
@@ -105,8 +134,13 @@ def plot_labeled_samples(samples, sample_classes=None, text_labels=None, fname=N
 def plot_gan_samples(gan, limit, fname=None, size=500):
     """Scatter size samples of the gan: no evaluation"""
     samples, _ = gan.sample_goals(size)
-    plt.clf()
-    plt.scatter(samples[:, 0], samples[:, 1])
+    fig = plt.figure()
+    # plt.clf()
+    if np.size(samples[0]) >= 3:
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(samples[:, 0], samples[:, 1], samples[:, 2])
+    else:
+        plt.scatter(samples[:, 0], samples[:, 1])
     plt.ylim(-limit, limit)
     plt.xlim(-limit, limit)
     if fname is not None:
