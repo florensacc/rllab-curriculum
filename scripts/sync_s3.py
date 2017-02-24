54ec47defacda78aeee87ba51c491747c261d22d
florensacc <florensacc@berkeley.edu>
2017-02-23 19:29:36 -0800

    do not sync itr folders

--- scripts/sync_s3.py
+++ scripts/sync_s3.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     parser.add_argument('folder', type=str, default=None, nargs='?')
     parser.add_argument('--dry', action='store_true', default=False)
     parser.add_argument('--bare', action='store_true', default=False)
+    parser.add_argument('--noitr', action='store_true', default=False)
     parser.add_argument('--all', action='store_true', default=False)
     args = parser.parse_args()
     remote_dir = config.AWS_S3_PATH
@@ -17,18 +18,13 @@ if __name__ == "__main__":
     if args.folder:
         remote_dir = os.path.join(remote_dir, args.folder)
         local_dir = os.path.join(local_dir, args.folder)
+    command = """aws s3 sync {remote_dir} {local_dir} --content-type "UTF-8" """.format(local_dir=local_dir, remote_dir=remote_dir)
+    if args.noitr:
+        command += """ --exclude '*itr*' """
     if args.bare:
-        command = ("""
-            aws s3 sync {remote_dir} {local_dir} --exclude '*' --include '*.csv' --include '*.json' --content-type "UTF-8"
-        """.format(local_dir=local_dir, remote_dir=remote_dir))
+        command += """ --exclude '*' --include '*.csv' --include '*.json' """
     elif not args.all:
-        command = ("""
-            aws s3 sync {remote_dir} {local_dir} --exclude '*stdout.log' --exclude '*stdouterr.log' --content-type "UTF-8"
-        """.format(local_dir=local_dir, remote_dir=remote_dir))
-    else:
-        command = ("""
-            aws s3 sync {remote_dir} {local_dir}  --content-type "UTF-8"
-        """.format(local_dir=local_dir, remote_dir=remote_dir))
+        command += """ --exclude '*stdout.log' --exclude '*stdouterr.log'  """
     if args.dry:
         print(command)
     else:
