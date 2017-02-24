345f1d584a29ba19e57d959e09165915a237cfcb
Xinyang Geng <young.gengxy@gmail.com>
2017-02-24 03:18:37 -0800

    fixed get goal observation problem

--- sandbox/young_clgan/lib/envs/maze/maze_env.py
+++ sandbox/young_clgan/lib/envs/maze/maze_env.py
@@ -139,8 +139,10 @@ class MazeEnv(GoalEnv, ProxyEnv, Serializable):
         return np.concatenate([self.wrapped_env.get_current_obs(),
                                ])
 
+    @overrides
+    @property
     def goal_observation(self):
-        return self.wrapped_env.goal_observation()
+        return self.wrapped_env.goal_observation
 
     def get_ori(self):
         """
--- sandbox/young_clgan/lib/envs/maze/point_env.py
+++ sandbox/young_clgan/lib/envs/maze/point_env.py
@@ -88,6 +88,8 @@ class PointEnv(GoalEnv, MujocoEnv, Serializable):
             distance=dist,
         )
 
+    @overrides
+    @property
     def goal_observation(self):
         return self.get_body_com("torso")[:2]
 
