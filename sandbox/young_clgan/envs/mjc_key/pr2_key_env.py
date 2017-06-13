496819ebfd33f990292e8b1f38662f10fd9ec11f
Markus Wulfmeier <markus@airbears2-10-142-37-29.airbears2.1918.berkeley.edu>
2017-06-13 16:38:29 -0700

    Setup pr2 key env

--- sandbox/young_clgan/envs/mjc_key/pr2_key_env.py
+++ sandbox/young_clgan/envs/mjc_key/pr2_key_env.py
@@ -8,40 +8,43 @@ from rllab.misc import autoargs
 
 
 class PR2KeyEnv(MujocoEnv, Serializable):
-
     FILE = 'pr2_arm3d_key.xml'
 
     @autoargs.arg('ctrl_cost_coeff', type=float,
                   help='cost coefficient for controls')
     def __init__(
             self,
-            ctrl_cost_coeff=1e-2,
-            goal_dist = 1e-1,
+            ctrl_cost_coeff=1e-0,
+            goal_dist=1e-0,
             *args, **kwargs):
         self.ctrl_cost_coeff = ctrl_cost_coeff
         super(PR2KeyEnv, self).__init__(*args, **kwargs)
         Serializable.quick_init(self, locals())
         self.goal_dist = goal_dist
-        # todo adapt to true goal position
-        self.goal_position = 1.
+        self.ee_indices = [14,23]
+        self.frame_skip = 5
+        self.init_qpos = np.array([0.1, 0.1, -1.54, -1.7, 1.54, -0.2, 0])
 
         # todo information from hyperparams to add
         # initial psition
         # 'x0': [np.concatenate([np.array([0.1, 0.1, -1.54, -1.7, 1.54, -0.2, 0]),
         #                        np.zeros(7)])]
-        # theta = -np.pi / 2
-        # d = 0.15
-        # PR2_GAINS = np.array([3.09, 1.08, 0.393, 0.674, 0.111, 0.152, 0.098])
-        # # EE_TGT = np.array([0.0, 0.3, -0.5,  0.0, 0.3, -0.2, 0.0, 0.45, -0.35])
-        # EE_TGT = np.array(
-        #     [0.0, 0.3, -0.45 - d, 0.0, 0.3, -0.15 - d, 0.0 + 0.15 * np.sin(theta), 0.3 + 0.15 * np.cos(theta),
-        #      -0.3 - d])
-        # WP = np.array([1, 1, 1, 1, 1, 1, 0, 0, 0])
+        theta = -np.pi / 2
+        d = 0.15
+        self.goal_position = np.array(
+            [0.0, 0.3, -0.45 - d, 0.0, 0.3, -0.15 - d, 0.0 + 0.15 * np.sin(theta), 0.3 + 0.15 * np.cos(theta),
+             -0.3 - d])
+        self.cost_params = {
+            'wp': np.array([1, 1, 1, 1, 1, 1, 1, 1, 1]),
+            'l1': 0.1,
+            'l2': 10.0,
+            'alpha': 1e-5}
 
     def get_current_obs(self):
         return np.concatenate([
             self.model.data.qpos.flat,
             self.model.data.qvel.flat,
+            self.model.data.site_xpos.flat,
         ]).reshape(-1)
 
     def step(self, action):
@@ -52,12 +55,15 @@ class PR2KeyEnv(MujocoEnv, Serializable):
         ctrl_cost = 0.5 * self.ctrl_cost_coeff * np.sum(
             np.square(action / scaling))
         # todo check which object has to be at goal position
-        key_position = self.get_body_com('key_head1')
-        dist = np.sqrt(np.sum(np.square(self.goal_position-key_position)))
-        if dist < self.goal_dist:
-            dist_cost = 0 #- dist
-        else:
-            dist_cost = - 1.# self.goal_dist
-        reward = dist_cost - ctrl_cost
+        # todo also check the meaning of alpha
+        # key_position = self.get_body_com('key_head1')
+        ee_position = next_obs[self.ee_indices[0]:self.ee_indices[1]]
+        dist = np.sum(np.square(self.goal_position - ee_position) * self.cost_params['wp'])
+        dist_cost = np.sqrt(dist) * self.cost_params['l1'] + dist * self.cost_params['l2']
+        # if dist < self.goal_dist:
+        #     dist_cost = 0  # - dist
+        # else:
+        #     dist_cost = - 1.  # self.goal_dist
+        reward = - dist_cost - ctrl_cost
         done = True if dist < self.goal_dist else False
         return Step(next_obs, reward, done)
