effc3629fe91095220a0b43ea1ea4fe9e52ef255
florensacc <florensacc@berkeley.edu>
2017-07-26 16:34:22 -0700

    parallelize Collection append

--- sandbox/young_clgan/state/utils.py
+++ sandbox/young_clgan/state/utils.py
@@ -1,6 +1,9 @@
+import multiprocessing
+from rllab.sampler.stateful_pool import singleton_pool
 import scipy.spatial
 import random
 from rllab import spaces
+from rllab.misc import logger
 import sys
 import os.path as osp
 
@@ -12,6 +15,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import scipy.misc
+from sandbox.young_clgan.state.evaluator import parallel_map, disable_cuda_initializer
 
 
 class StateCollection(object):
@@ -37,12 +41,48 @@ class StateCollection(object):
             states += replay_noise * np.random.randn(*states.shape)
         return states
 
+    def append(self, states, n_process=None):
+        if self.states_transform:
+            return self.append_states_transform(states)
+        if len(states) > 0:
+            states = np.array(states)
+            logger.log("we are trying to append states: {}".format(states.shape))
+            if self.distance_threshold is not None and self.distance_threshold > 0:
+                states = self._process_states(states)
+            logger.log("after processing, we are left with : {}".format(states.shape))
+            if n_process is None:
+                n_process = singleton_pool.n_parallel
+            elif n_process in [-1, 0]:
+                n_process = 1
+            if n_process > 1:
+                states_per_process = states.shape[0] // singleton_pool.n_parallel
+                list_of_states = [states[i * states_per_process: (i+1) * states_per_process, :] for i in range(n_process-1)]
+                list_of_states.append(states[states_per_process * (n_process - 1):, :])
+                states = parallel_map(self._select_states, list_of_states)
+                states = np.concatenate(states)
+            else:
+                states = self._select_states(states)
+            self.state_list.extend(states.tolist())
+            return states
+
+    def _select_states(self, states):
+        # print('selecting states from ', states.shape)
+        selected_states = states
+        if self.distance_threshold is not None and self.distance_threshold > 0:
+            if len(self.state_list) > 0:
+                dists = scipy.spatial.distance.cdist(self.state_list, selected_states)
+                indices = np.amin(dists, axis=0) > self.distance_threshold
+                selected_states = selected_states[indices, :]
+        # print('the selected states are: {}'.format(selected_states.shape))
+        return selected_states
+
     def _process_states(self, states):
         "keep only the states that are at more than dist_threshold from each other"
         # adding a states transform allows you to maintain full state information while possibly disregarding some dim
         states = np.array(states)
         results = [states[0]]
-        for state in states[1:]:
+        for i, state in enumerate(states[1:]):
+            # print("analyzing state : ", i)
             if np.amin(scipy.spatial.distance.cdist(results, state.reshape(1, -1))) > self.distance_threshold:
                 results.append(state)
         return np.array(results)
@@ -76,19 +116,6 @@ class StateCollection(object):
             assert(len(self.state_list) == len(self.transformed_state_list))
             return states # modifed to return added states
 
-    def append(self, states):
-        if self.states_transform:
-            return self.append_states_transform(states)
-        if len(states) > 0:
-            states = np.array(states)
-            if self.distance_threshold is not None and self.distance_threshold > 0:
-                states = self._process_states(states)
-                if len(self.state_list) > 0:
-                    dists = scipy.spatial.distance.cdist(self.state_list, states)
-                    indices = np.amin(dists, axis=0) > self.distance_threshold
-                    states = states[indices, :]
-            self.state_list.extend(states)
-            return states # modifed to return added states
 
     @property
     def states(self):
